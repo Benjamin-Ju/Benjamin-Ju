8b3dc7f1dc88b02126f7de9c1f2c9d45ee625230
Benjamin Ju <benjaminyanju@gmail.com>
2019-05-06 06:51:41 -0400

    Added new components, added nav bar, added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule } from 'ngx-bootstrap';
+import { NotesComponent } from './notes/notes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent },
+      { path: 'notes', component: NotesComponent },
+      { path: '**', component: NotFoundComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
